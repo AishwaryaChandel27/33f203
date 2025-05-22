68ac60cae65e5202d872468b1417076542c3acf2
Aishwarya Chandel <138582265+AishwaryaChandel27@users.noreply.github.com>
2025-05-22 22:52:31 +0530

    Update formApi.ts

--- src/api/formApi.ts
+++ src/api/formApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { parseGraph } from "../utlis/parseGraph";
 import type { FormNode , FormGraphRaw } from "../types";
 
-const API_URL = "http://localhost:3001/api/v1/anyAppId/actions/blueprints/anyFlowId/graph";
+const API_URL = "http://localhost:3000/api/v1/anyAppId/actions/blueprints/anyFlowId/graph";
 
 export const fetchFormGraph = async():
 Promise<FormNode[]> =>{
